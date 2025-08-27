1869a02f36009847d11f6d822395eaa9bb81b2bd
WSCWNeel <159732843+WSCWNeel@users.noreply.github.com>
2025-08-27 18:04:48 +1200

    .

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ window.addEventListener('load', function() {
     preloader.style.display = 'none'; // Hide the preloader
     }
 });
+
+function About_Us() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/about_us.html");
+}
+function Year_12_Resource_Website() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/index.html");
+}
+function How_to_use_the_Website() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/how_to_use_the_website.html");
+}
+function myMaths() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/advanced_maths.html");
+}
+function myPhysics() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/physics.html");
+}
+function myEnglish() {
+window.location.href = ("https://wscwneel.github.io/Year-12-Learning-Resources-Website/english.html");
+}
